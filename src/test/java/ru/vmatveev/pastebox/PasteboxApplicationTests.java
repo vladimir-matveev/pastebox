5fdc1f97024e6b697f4ecef76531f8634305713e
Vladimir Matveev <ru.matveev.v@gmail.com>
2024-10-03 12:13:06 +0300

    Init

---
+++ src/test/java/ru/vmatveev/pastebox/PasteboxApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vmatveev.pastebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
